efcf9776c6e5a17cbfb13e23fc9a1bab02ac8143
Aiperi Anarbekovna <ajperikamcybekova146@gmail.com>
2024-02-28 14:11:51 +0600

    p

--- main.js
+++ main.js
@@ -37,3 +37,69 @@ kgBtn.addEventListener('click', () => {
 
 
 updateGreeting();
+// 2. Положить в проект JSON-файлы, каждый из которых соответствует одному из языков. Внутри JSON-данных описаны одни и те же слова (например "человек", "дом", "хлеб") на соответствующем языке.
+JSON = {
+    "man": "man",
+    "house": "house",
+    "bread": "bread"
+}
+ 
+JSON ={
+    "man": "человек",
+    "house": "дом",
+    "bread": "хлеб"
+}
+
+JSON = {
+    "man": "адам",
+    "house": "үй",
+    "bread": "нан"
+}
+
+
+const languages = ['en', 'ru', 'kg'];
+let currentLanguage1 = 'en';
+let dictionary = {};
+
+
+async function loadLanguage(language) {
+    const response = await fetch(`${language}.json`);
+    if (!response.ok) {
+        throw new Error(`Failed to load language data for ${language}`);
+    }
+    const data = await response.json();
+    dictionary[language] = data;
+}
+
+
+async function loadLanguages() {
+    try {
+        await Promise.all(languages.map(loadLanguage));
+        updateGreeting();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+function updateGreeting() {
+    greeting.textContent = dictionary[currentLanguage]['man']; 
+}
+
+
+enBtn.addEventListener('click', () => {
+    currentLanguage = 'en';
+    updateGreeting();
+});
+
+ruBtn.addEventListener('click', () => {
+    currentLanguage = 'ru';
+    updateGreeting();
+});
+
+kgBtn.addEventListener('click', () => {
+    currentLanguage = 'kg';
+    updateGreeting();
+});
+
+loadLanguages();
